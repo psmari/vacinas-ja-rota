9c3a57b32dd6c9a02875277769df12a1d9a798d3
Neto <p.oliveirapo9@gmail.com>
2021-09-02 10:14:20 -0300

    Profundidade - Unitaria e Multi

--- main.py
+++ main.py
@@ -3,16 +3,32 @@ from copy import deepcopy
 
 sol = busca()
 
-# PROBLEMA COM DESTINO ÚNICO
+# AMPLITUDE - PROBLEMA COM DESTINO ÚNICO
 origem = "ALENCAR"
 destino = "SILVA"
 caminho = sol.amplitudeUnitario(origem, destino)
 print("\n> Amplitude com destino único: ", caminho)
 
-# PROBLEMA COM MAIS DE UM DESTINO
+# AMPLITUDE - PROBLEMA COM MAIS DE UM DESTINO
 origem  = "ALENCAR"
 destino = ["WAGNER","YORK","SILVA"]
 destino1 = deepcopy(destino)
 
 caminho = sol.amplitudeMulti(origem, destino1)
-print("\n> Amplitude com destino único múltiplo: ", caminho)
+print("\n> Amplitude com destino múltiplo: ", caminho)
+
+# PROFUNDIDADE - PROBLEMA COM DESTINO ÚNICO
+origem = "ALENCAR"
+destino = "SILVA"
+
+caminho = sol.profundidadeUnitario(origem,destino)
+print("\n> Profundidade com destino único: ", caminho)
+
+# PROFUNDIDADE  - PROBLEMA COM MAIS DE UM DESTINO
+origem  = "ALENCAR"
+destino = ["WAGNER","YORK","SILVA"]
+destino1 = deepcopy(destino)
+
+caminho = sol.profundidadeMulti(origem,destino1)
+print("\n> Profundidade com destino múltiplo: ", caminho)
+
