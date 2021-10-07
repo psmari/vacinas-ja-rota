6f315bd7aff731ee5187c9d0797179e391bccdae
Mafscn <marco.afs.dev@gmail.com>
2021-10-07 11:40:52 -0300

    super melhorias na interface!

--- view/buildGrafo.py
+++ view/buildGrafo.py
@@ -1,12 +1,11 @@
 from tkinter import *
+from view.colors import *
 from view.grafo import *
 from view.interface import *
 
 
 def buildGrafo(root, caminho, background):
-    global grafoFrame
-
-    grafoFrame = LabelFrame(root, text=" Grafo ")
+    grafoFrame = Frame(root, background=CLR_BACKGROUND)
 
     Label(grafoFrame, image=background).grid(column=1, row=1, columnspan=16, rowspan=14)
 
@@ -15,9 +14,9 @@ def buildGrafo(root, caminho, background):
             Label(
                 grafoFrame,
                 text=f" {posto[2]} ",
-                foreground="white",
-                background="#1F8A70",
                 font=("TkDefaultFont", 9, "bold"),
+                foreground="white",
+                background=CLR_DEFAULT,
             ).grid(column=posto[1], row=posto[0])
 
             for lugar in caminho:
@@ -26,16 +25,16 @@ def buildGrafo(root, caminho, background):
                         grafoFrame,
                         text=f" {posto[2]} ",
                         foreground="white",
-                        background="#BEDB39",
+                        background=CLR_SELECTED,
                         font=("TkDefaultFont", 9, "bold"),
                     ).grid(column=posto[1], row=posto[0])
     else:
         Label(
             grafoFrame,
             text="\n   Caminho não encontrado   \n",
-            foreground="white",
-            background="#FD7400",
+            foreground=CLR_FONT,
+            background=CLR_ALERT,
             font=("TkDefaultFont", 14, "bold"),
         ).grid(column=8, row=7)
 
-    grafoFrame.grid(column=1, row=0, rowspan=3, padx=4, pady=4)
+    grafoFrame.grid(column=1, row=0)
--- view/interface.py
+++ view/interface.py
@@ -2,34 +2,42 @@ from tkinter import *
 from PIL import ImageTk, Image
 from busca import busca
 from dados import nos
+from view.colors import *
 from view.grafo import *
-from view.buildGrafo import *
+from view.buildGrafo import buildGrafo
 
 sol = busca()
 
 
-def btnFunc(btn, origem, destino):
-    if btn == "Amplitude Unitario":
-        return sol.amplitudeUnitario(origem, destino)
-    elif btn == "Profundidade Unitario":
-        return sol.profundidadeUnitario(origem, destino)
-    elif btn == "Amplitude Multipla":
-        return sol.amplitudeMulti(origem, [destino])
-    elif btn == "Profundidae Multipla":
-        return sol.profundidadeMulti(origem, [destino])
-    elif btn == "Profundidade Limitada":
-        return sol.profundidade_limitada(origem, destino, 5)
-    elif btn == "Aprofundamento Iterativo":
-        return sol.aprofundamento_iterativo(origem, destino)
-    elif btn == "Bidirecional":
-        return sol.bidirecional(origem, destino)
+btnNames = [
+    "Amplitude Unitario",
+    "Profundidade Unitario",
+    "Amplitude Multipla",
+    "Profundidade Multipla",
+    "Profundidade Limitada",
+    "Aprofundamento Iterativo",
+    "Bidirecional",
+]
+
+
+def botoes(btn, origem, destino):
+    switcher = {
+        0: sol.amplitudeUnitario(origem, destino),
+        1: sol.profundidadeUnitario(origem, destino),
+        2: sol.amplitudeMulti(origem, [destino]),
+        3: sol.profundidadeMulti(origem, [destino]),
+        4: sol.profundidade_limitada(origem, destino, 5),
+        5: sol.aprofundamento_iterativo(origem, destino),
+        6: sol.bidirecional(origem, destino),
+    }
+
+    return switcher.get(btn, "nothing")
 
 
 def buildGUI():
     root = Tk()
     root.title("VacinasJá")
-
-    global grafoFrame
+    root.config(background=CLR_BACKGROUND)
 
     background = ImageTk.PhotoImage(Image.open("./view/sprites/graph.png"))
     origem = StringVar(root)
@@ -38,48 +46,95 @@ def buildGUI():
     destino.set("DUARTE")
 
     # FRAMES
-    origemFrame = LabelFrame(root, text=" Origem ", padx=4, pady=4)
-    destinoFrame = LabelFrame(root, text=" Origem ", padx=4, pady=4)
-    opcoesFrame = LabelFrame(root, text=" Algoritimos ", padx=4, pady=4)
-    grafoFrame = LabelFrame(root, text=" Grafo ")
+    menuFrame = Frame(
+        root,
+        background=CLR_BACKGROUND,
+    )
+    origemFrame = LabelFrame(
+        menuFrame,
+        padx=4,
+        pady=4,
+        text=" Origem ",
+        font=("TkDefaultFont", 12, "bold"),
+        foreground=CLR_FONT,
+        background=CLR_BACKGROUND,
+    )
+    destinoFrame = LabelFrame(
+        menuFrame,
+        padx=4,
+        pady=4,
+        text=" Destino ",
+        font=("TkDefaultFont", 12, "bold"),
+        foreground=CLR_FONT,
+        background=CLR_BACKGROUND,
+    )
+    opcoesFrame = LabelFrame(
+        menuFrame,
+        padx=4,
+        pady=4,
+        text=" Algoritimos ",
+        font=("TkDefaultFont", 12, "bold"),
+        foreground=CLR_FONT,
+        background=CLR_BACKGROUND,
+    )
+    grafoFrame = Frame(
+        root,
+        background=CLR_BACKGROUND,
+    )
 
     # INPUT
     origemField = OptionMenu(origemFrame, origem, *nos)
-    origemField.config(width=16)
-    origemField.grid(column=0, row=0)
+    origemField.config(
+        width=20,
+        font=("TkDefaultFont", 9, "bold"),
+        activeforeground=CLR_DEFAULT,
+    )
+    origemField.grid(column=0, row=0, padx=4, pady=4)
 
     destinoField = OptionMenu(destinoFrame, destino, *nos)
-    destinoField.config(width=16)
-    destinoField.grid(column=0, row=1)
+    destinoField.config(
+        width=20,
+        font=("TkDefaultFont", 9, "bold"),
+        activeforeground=CLR_DEFAULT,
+    )
+    destinoField.grid(column=0, row=1, padx=4, pady=4)
 
-    # OPÇÕES
-    botaoNum = 0
-    botoes = [
-        "Amplitude Unitario",
-        "Profundidade Unitario",
-        "Amplitude Multipla",
-        "Profundidae Multipla",
-        "Profundidade Limitada",
-        "Aprofundamento Iterativo",
-        "Bidirecional",
-    ]
-
-    for botao in botoes:
+    # BOTOES
+    for botao in range(6):
         Button(
             opcoesFrame,
-            width=24,
-            text=botao,
-            command=lambda botao=botao: buildGrafo(
-                root, btnFunc(botao, origem.get(), destino.get()), background
+            width=22,
+            text=btnNames[botao],
+            font=("TkDefaultFont", 9, "bold"),
+            activeforeground=CLR_DEFAULT,
+            command=lambda botao=botao, origem=origem, destino=destino: buildGrafo(
+                root, botoes(botao, origem.get(), destino.get()), background
             ),
-        ).grid(column=0, row=botaoNum)
+        ).grid(column=0, row=botao, padx=4, pady=4)
 
-        botaoNum += 1
+    # DESENVOLVEDORES
+    nomes = Label(
+        menuFrame,
+        text="Joel dos Anjos\n"
+        + "\nMarco Antônio\n"
+        + "\nMariana Pereira\n"
+        + "\nPedro Luis",
+        font=("TkDefaultFont", 9, "bold"),
+        anchor="w",
+        justify=LEFT,
+        foreground=CLR_FONT,
+        background=CLR_BACKGROUND,
+    )
+
+    # GRAFO BACKGROUND
+    Label(grafoFrame, image=background).grid(column=1, row=1, columnspan=16, rowspan=14)
 
     # BUILD INTERFACE
-    origemFrame.grid(column=0, row=0, padx=(4, 2), pady=4)
-    destinoFrame.grid(column=0, row=1, padx=(4, 2), pady=4)
-    opcoesFrame.grid(column=0, row=2, padx=(4, 2), pady=4)
-    buildGrafo(root, btnFunc(botoes[0], origem.get(), destino.get()), background)
+    origemFrame.pack(padx=8, pady=8)
+    destinoFrame.pack(padx=8, pady=8)
+    opcoesFrame.pack(padx=8, pady=8)
+    nomes.pack(padx=12, pady=8, fill=BOTH)
+    menuFrame.grid(column=0, row=0)
+    grafoFrame.grid(column=1, row=0)
 
     root.mainloop()
